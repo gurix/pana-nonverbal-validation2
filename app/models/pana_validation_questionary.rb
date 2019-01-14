1c1d95a2363a21303fa4f634ddd997e60c9bf770
Markus Graf <info@markusgraf.ch>
2019-01-14 13:32:52 +0100

    Added texts and enhanced styling for mobile.

--- app/models/pana_validation_questionary.rb
+++ app/models/pana_validation_questionary.rb
@@ -2,14 +2,14 @@ class PanaValidationQuestionary < ApplicationRecord
   belongs_to :subject
 
   # Validate presence for each pair of emojis
-  PanaValidationQuestionaryStructure.calculate_vertical_pana_columns.each do |column|
-    validates(column, presence: true)
+  PanaValidationQuestionaryStructure.calculate_vertical_pana_columns.each_with_index do |column, column_index|
+    validates(column, presence: true, if: proc { |q| q.page == (column_index + 1)})
   end
 
   # Validate presence for each item of the rest of the survey
   PanaValidationQuestionaryStructure.structure.each_with_index do |page, page_index|
     page.each do |item|
-      validates(item, presence: true, if: proc { |q| q.page == (page_index + 2) })
+      validates(item, presence: true, if: proc { |q| q.page == (page_index + 6) })
     end
   end
 
