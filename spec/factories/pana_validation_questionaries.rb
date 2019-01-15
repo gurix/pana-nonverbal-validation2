f0bd2ad0ab9cd776dee424f29d16458d7ac854ad
Markus Graf <info@markusgraf.ch>
2019-01-15 09:26:23 +0100

    fixed export test

--- spec/factories/pana_validation_questionaries.rb
+++ spec/factories/pana_validation_questionaries.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :pana_validation_questionary do
     5.times { |i| add_attribute("who#{i + 1}") { i + 1 } }
     10.times { |i| add_attribute("pnv#{i + 1}") { i + 1 } }
-    5.times { |i| add_attribute("swl#{i + 1}") { i + 1 } }
+    5.times { |i| add_attribute("swls#{i + 1}") { i + 1 } }
     2.times { |i| add_attribute("emoji_matrix_#{i + 1}") { i + 1 } }
 
     PanaValidationQuestionaryStructure.calculate_vertical_pana_columns.each_with_index do |name, index|
--- spec/features/export/show_spec.rb
+++ spec/features/export/show_spec.rb
@@ -15,10 +15,10 @@ feature 'Export page' do
 
     visit export_path
     # Testing header
-    expect(page).to have_content 'id,age,gender,education,residence,created_at,tic,subject_id,page,updated_at,who1,who2,who3,who4,who5,mrs1,mrs2,mrs3,mrs4,mrs5,mrs6,mrs7,mrs8,mrs9,mrs10,mrs11,mrs12,mrs13,mrs14,mrs15,mrs16,mrs17,mrs18,mrs19,mrs20,pnv1,pnv2,pnv3,pnv4,pnv5,pnv6,pnv7,pnv8,pnv9,pnv10,swls1,swls2,swls3,swls4,swls5,sam1,sam2,sam3,PA3_lo__PA1_hi,NA2_lo__NA1_hi,PA4_hi__PA4_lo3,VA1_lo__NA3_lo,VA1_hi__VA2_lo,PA1_lo__PA1_hi,NA3_lo__NA2_hi,PA3_hi__PA3_lo,NA1_hi__NA1_lo,VA2_hi__VA2_lo,VA1_hi__VA1_lo,PA1_hi__PA1_lo,PA2_lo__PA2_hi1,NA2_lo__NA2_hi,VA2_lo__VA2_hi,PA3_lo__PA3_hi,NA3_lo__NA3_hi,NA4_hi__NA4_lo,PA2_lo__PA2_hi5,pan1,pan2,pan3,pan4,pan5,pan6,pan7,pan8,pan9,pan10,pan11,pan12,pan13,pan14,pan15,pan16,pan17,pan18,pan19,pan20,visit_token,visitor_token,ip,user_agent,referrer,referring_domain,landing_page,browser,os,device_type,country,region,city,utm_source,utm_medium,utm_term,utm_content,utm_campaign,started_at' # rubocop:disable Metrics/LineLength
+    expect(page).to have_content 'id,age,gender,education,residence,created_at,tic,subject_id,page,updated_at,VA2_hi__VA2_lo,PA3_hi__PA3_lo,NA3_hi__NA3_lo,PA2_hi1__PA2_lo,NA2_hi__NA2_lo,who1,who2,who3,who4,who5,pnv1,pnv2,pnv3,pnv4,pnv5,pnv6,pnv7,pnv8,pnv9,pnv10,emoji_matrix_1,emoji_matrix_2,swls1,swls2,swls3,swls4,swls5,visit_token,visitor_token,ip,user_agent,referrer,landing_page,referring_domain,search_keyword,browser,os,device_type,screen_height,screen_width,country,region,city,postal_code,latitude,longitude,utm_source,utm_medium,utm_term,utm_content,utm_campaign,started_a' # rubocop:disable Metrics/LineLength
 
     # Testing if id and age is included in two rows everything else is not testet at the moment.
-    expect(page).to have_content '1,42'
-    expect(page).to have_content '4,42'
+    expect(page).to have_content '1,42,f,1,ch'
+    expect(page).to have_content '4,42,f,1,ch'
   end
 end
