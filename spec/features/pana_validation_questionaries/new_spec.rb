4034838aa682a99ae113b99a1fcdb16bacf0c5a1
Markus Graf <info@markusgraf.ch>
2019-02-07 16:46:28 +0100

    Added tests

--- spec/features/pana_validation_questionaries/new_spec.rb
+++ spec/features/pana_validation_questionaries/new_spec.rb
@@ -20,33 +20,33 @@ def standard_questionary(subject) # rubocop:disable Metrics/MethodLength
     expect(subject.reload.pana_validation_questionary[column]).to eq new_value
   end
 
-  # Page 6
-  expect(page).to have_selector 'h1', text: 'Die folgenden Aussagen betreffen Ihr Wohlbefinden in den letzten Tagen.'
-
+  # Page 6 to 10
   5.times do |i|
-    choose "pana_validation_questionary_who#{i + 1}_2"
-  end
+    expect(page).to have_selector 'h1', text: 'Die folgenden Aussage betrifft Ihr Wohlbefinden in den letzten Tagen.'
 
-  click_button 'Weiter'
+    choose "pana_validation_questionary_who#{i + 1}_2"
+
+    click_button 'Weiter'
+  end
 
   5.times do |i|
     expect(subject.reload.pana_validation_questionary["who#{i + 1}"]).to eq 2
   end
 
-  # Page 7
-  expect(page).to have_selector 'h1', text: 'Wie fühlten Sie sich in den letzten Tagen?'
-
+  # Page 11 to 20
   10.times do |i|
-    choose "pana_validation_questionary_pnv#{i + 1}_7"
-  end
+    expect(page).to have_selector 'h1', text: 'Wie fühlten Sie sich in den letzten Tagen?'
 
-  click_button 'Weiter'
+    choose "pana_validation_questionary_pnv#{i + 1}_7"
+
+    click_button 'Weiter'
+  end
 
   10.times do |i|
     expect(subject.reload.pana_validation_questionary["pnv#{i + 1}"]).to eq 7
   end
 
-  # Page 8 and 9
+  # Page 21 and 22
   PanaValidationQuestionaryStructure::MATRIX_MAPPING.each_with_index do |row, index|
     item = row.flatten.sample
 
@@ -60,14 +60,17 @@ def standard_questionary(subject) # rubocop:disable Metrics/MethodLength
     expect(subject.reload.pana_validation_questionary["emoji_matrix_#{index + 1}"]).to eq item
   end
 
-  # Page 10
-  expect(page).to have_selector 'h1', text: 'Es folgen fünf Aussagen, denen Sie zustimmen bzw. die Sie ablehnen können.'
-
+  # Page 23 to 27
   5.times do |i|
+    expect(page).to have_selector 'h1', text: 'Folgende Aussage können Sie zustimmen bzw. ablehnen.'
+
     choose "pana_validation_questionary_swls#{i + 1}_3"
+
+    click_button 'Weiter'
+
+    expect(subject.reload.pana_validation_questionary["swls#{i + 1}"]).to eq 3
   end
 
-  click_button 'Weiter'
 end
 
 feature 'PanaValidationQuestionary' do
