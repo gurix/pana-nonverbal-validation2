fe00331c973a59c0faf5b73c4663971910756521
Markus Graf <info@markusgraf.ch>
2019-05-06 12:53:23 +0200

    Only allow screens of a certain size.

--- spec/features/home/show_spec.rb
+++ spec/features/home/show_spec.rb
@@ -5,4 +5,21 @@ feature 'Home page' do
     visit root_path(tic: 'LookingForFreedom')
     expect(page).to have_link 'Umfrage starten', href: new_subject_path(tic: 'LookingForFreedom')
   end
+
+  scenario 'Visit the homepage the resolution has to be at least 1000px', js: true do
+    visit root_path
+
+    expect(page).to have_content 'Entwicklung einer Emoji-Skala zur Erfassung positiver und negativer Aktivierung'
+    expect(page).not_to have_content 'Für diese Befragung ist Ihre Bildschirmauflösung zu gering.'
+
+    page.driver.browser.manage.window.resize_to(800, 800)
+
+    expect(page).not_to have_content 'Entwicklung einer Emoji-Skala zur Erfassung positiver und negativer Aktivierung'
+    expect(page).to have_content 'Für diese Befragung ist Ihre Bildschirmauflösung zu gering.'
+
+    page.driver.browser.manage.window.resize_to(1024, 800)
+
+    expect(page).to have_content 'Entwicklung einer Emoji-Skala zur Erfassung positiver und negativer Aktivierung'
+    expect(page).not_to have_content 'Für diese Befragung ist Ihre Bildschirmauflösung zu gering.'
+  end
 end
